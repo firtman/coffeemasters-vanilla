6276ca363150bcda504f5bf9c98502b6ff5c862e
Rajat Bansal <rjtbansal@gmail.com>
2024-02-05 15:35:47 +0700

    Section 2: The DOM lesson (#1)
    Coded along entire The DOM section.
    
    New services have been added including API call to fetch menu. No DOM is rendering yet but API has been called successfully.
    There is also some context regarding some concepts related to DOM and aliases which got deleted later

---
+++ app.js
@@ -0,0 +1,24 @@
+/**
+ * DOMContentLoaded event ensures that DOM has loaded entirely and ready to be  
+ * manipulated by DOM apis
+ * 
+ * Note that this is different than 'load' event where JS waits for entire page
+ * to be loaded including DOM, images, text and in most cases we dont want that
+ * if all we want is manipulate DOM
+ */
+
+import { loadData } from "./services/Menu.js";
+import Store from "./services/Store.js"; // put .js if import doesnt work
+
+/**Since Store is imported as a module below is a workaround to have access
+ * to Store via window object
+ * 
+ * We are attaching myapp (could be any name) to the window object
+ * and through that we are attaching store field and pointing it to our Store service
+ */
+window.myapp = {};
+myapp.store = Store;
+
+window.addEventListener('DOMContentLoaded', () => {
+  loadData();
+});
---
+++ services/Store.js
@@ -0,0 +1,10 @@
+/**
+ * We are trying to mimick a global store we need to store our 
+ * menu data and cart items
+ */
+const Store = {
+  menu: null,
+  cart: []
+};
+
+export default Store;
