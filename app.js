4e0963daeeeffded5031aeb5758862873f5a3374
Rajat Bansal <rjtbansal@gmail.com>
2024-02-05 13:31:06 +0700

    Added some initialization functions and aliases with explanation

--- app.js
+++ app.js
@@ -12,3 +12,24 @@ window.addEventListener('DOMContentLoaded', () => {
   <h1>This is jS induced nav</h1>
   `
 });
+
+/**
+ * 
+ * Below are examples of how flexible JS is
+ * 
+ * We are reducing verbosity of various functions by specifying aliases
+ * 
+ * Reference: https://firtman.github.io/vanilla/lessons/the-dom/initializing-project
+ */
+
+// Below means we could use $('nav') similar to JQuery
+const $ = function(args){ return document.querySelector(args);} 
+const $$ = function(args){ return document.querySelectorAll(args);}
+
+// $.on('click') will use addEventListener syntax 
+// we are allowed to tweak prototype as shown below to add our own abstractions!
+HTMLElement.prototype.on = function (a, b, c) { return this.addEventListener(a, b, c); }
+// $.off('click') will remove our event listener without syntax verbosity
+HTMLElement.prototype.off = function (a, b) { return this.removeEventListener(a, b); }
+HTMLElement.prototype.$ = function(s){ return this.querySelector(s); }
+HTMLElement.prototype.$$ = function(s){ return this.querySelectorAll(s); }
